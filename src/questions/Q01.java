0910bcc02985bddb79058f8afdd91e4d76a2257f
Yasemin DOĞAN <yasemin.dn7@gmail.com>
2023-05-13 11:56:55 +0300

    Q1

--- src/questions/Q01.java
+++ src/questions/Q01.java
@@ -6,17 +6,18 @@ import java.util.List;
 public class Q01 {
     public static void main(String[] args) {
 
-        int a[][] = { {3,6,2}, {1,5,6}, {2,1,5} };
+        int a[][] = { {3,6,2}, {1,5,6}, {2,1,5}, {9,2,1} };
         int b[][] = { {1,1,2}, {3,3,3}, {2,2,1} };
 
-        result(a, b);
+        System.out.println(result(a,b));
 
     }
 
     //A dizisinde olup B dizisinde olmayan elemanları bulunuz.
-    public static void result(int a[][], int b[][]){
+    public static List<Integer> result(int a[][], int b[][]){
 
        List<Integer> listB = new ArrayList<>();
+       List<Integer> listResult = new ArrayList<>();
 
         for (int[] w1 : a) {
             for (int i1 : w1) {
@@ -28,10 +29,12 @@ public class Q01 {
                 }
 
                 if(!listB.contains(i1)){
-                    System.out.print(i1 + " ");
+                    listResult.add(i1);
                 }
             }
         }
+
+        return listResult;
 
     }
 
---
+++ src/questions/Q03.java
@@ -0,0 +1,22 @@
+package questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q03 {
+    public static void main(String[] args) {
+
+        int a[][] = { {3,6,2}, {1,5,6}, {2,1,5} };
+
+        result(a);
+
+    }
+
+    //A dizinde her elemanın kaç defa geçtiğini bulunuz.
+    public static void result(int[][] a){
+
+
+
+    }
+
+}
